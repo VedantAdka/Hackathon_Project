85587dacb6d6fab06aec56db7dda9b9a9ee81319
Adka <2408439@cognizant.com>
2025-07-10 13:49:31 +0530

    1st commit

---
+++ src/test/java/com/cts/policy_bazaar/pageobjects/BasePage.java
@@ -0,0 +1,12 @@
+package com.cts.policy_bazaar.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+abstract public class BasePage {
+    protected WebDriver driver;
+    protected BasePage(WebDriver driver){
+        this.driver=driver;
+        PageFactory.initElements(driver,this);
+    }
+}
