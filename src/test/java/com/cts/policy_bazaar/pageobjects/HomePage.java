80c983ac448552432a622cf18fb574504c88ba3e
Vedant_Adka <vedantadka@gmail.com>
2025-07-15 09:04:07 +0530

    Final Changes

--- src/test/java/com/cts/policy_bazaar/pageobjects/HomePage.java
+++ src/test/java/com/cts/policy_bazaar/pageobjects/HomePage.java
@@ -4,23 +4,34 @@ import com.cts.policy_bazaar.frameworkutils.CommonUtils;
 import com.cts.policy_bazaar.seleniumutils.ActionUtil;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends BasePage{
-
+    Actions actions;
     @FindBy(xpath = "//div[7]//div[@class='shadowHandlerBox']")
     private WebElement travelInsurance;
+
+    @FindBy(xpath = "//p[text()='Car']")
+    WebElement carSection;
+
     @FindBy(xpath = "//ul[@class='ruby-menu']/li[2]/a")
     private WebElement insuranceProduct;
+
     @FindBy(xpath = "//div[@class='ruby-row']/div[3]/h3")
     private  WebElement healthInsurance;
 
     public HomePage(WebDriver driver){
         super(driver);
+        actions = new Actions(driver);
     }
     public void clickOnTravelInsurance(){
-        CommonUtils.sureWait(1);
         travelInsurance.click();
+    }
+    public void clickCarSection() {
+        actions.moveToElement(carSection).click().perform();
+        driver.manage().deleteAllCookies();
+        CommonUtils.sureWait(2);
     }
     public boolean insuranceProductsIsEnabled(){
         return insuranceProduct.isEnabled();
@@ -37,5 +48,6 @@ public class HomePage extends BasePage{
         ActionUtil.moveToElementAction(driver,healthInsurance);
         ActionUtil.clickAction(driver,healthInsurance);
     }
-
 }
+
+
